40ca4fbc61eb857f35136819e6e8a112ead9be63
TermFerd <tford2035@gmail.com>
2022-02-16 21:02:32 +0000

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, numberOfTimes) {
+	 let repeatedString = 'hey';
+     let numberOfTimes = 3;
+     let i;
+     for(i = 0; i < numberOfTimes - 1; i++) {
+        repeatedString += str;
+      }
+     return repeatedString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;

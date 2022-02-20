de2e34b9fa700b1180d84d44fbfc708e74a23a43
TermFerd <tford2035@gmail.com>
2022-02-20 20:55:07 +0000

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+	let removed = arr.splice(1,0);
+	return removed;
 };
+
+removeFromArray()
 
 // Do not edit below this line
 module.exports = removeFromArray;
